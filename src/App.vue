d65647ec1a9f4525ca9b6d4185b1dbb99002d4e9
Daniel Kock <daniel.kock@mercadolivre.com>
2020-10-14 20:13:17 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <counter/>
+    </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+
+import Counter from './vue/counter.vue'
+
+export default {
+    name: 'App',
+    components: { Counter },
+    data () {
+        return {
+        }
+    },
+};
+
+</script>
+
+
+<style lang="scss">
+    
+</style>
---
+++ src/vue/counter.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>{{timeNow}}</div>
+</template>
+
+<script>
+import moment from 'moment'
+
+
+export default {
+  name: 'counter',
+  data: function() {
+    return {
+    }
+  },
+  computed: {
+    timeNow: function() {
+      return moment().format('LTS');
+    }
+  }
+}
+</script>
+
+<style>
+</style>
